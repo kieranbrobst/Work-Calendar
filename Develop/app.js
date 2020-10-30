fd063e6b8e6b2ff35677100592893dfbbf6680e6
Kieran Brobst <kieranbrobst@gmail.com>
2020-10-30 15:34:09 -0700

    fixed local storage and added moments

--- Develop/app.js
+++ Develop/app.js
@@ -1,21 +1,28 @@
-// local storage
-// moments library
-// conditionals
+
 
 $(document).ready(function (){
-    const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
-    $("#currentTime").text(currentTime);
+    const currentTime = moment().format('dddd, MMMM Do, h:mm a');
+    $("#currentDay").text(currentTime);
 
 
-    const times = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+    const times = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
     
     times.forEach (time => {
-        const timeCheck = window.localStorage.getItem(time)
-        console.log(timeCheck)
+        const timeCheck = window.localStorage.getItem(time);
+        const currentHour = moment().hour();
+
+        if (currentHour > time) {
+            $(`#${time}`).addClass("past")
+            $(`#${time}`).attr("disabled", true)
+        } else if (currentHour === time) {
+            $(`#${time}`).addClass("present")
+        } else {
+            $(`#${time}`).addClass("future")
+        }
 
         if(timeCheck === null){
-            window.localStorage.setItem(time, "")
-        } else if (timeCheck.length > 0){
+            window.localStorage.setItem(time, "");
+        } else if (timeCheck.length > 0) {
             $(`#${time}`).attr("value", window.localStorage.getItem(time))
         }
 
@@ -23,8 +30,10 @@ $(document).ready(function (){
 
     $("form").on("submit", function (e){
         e.preventDefault();
-        console.log(e.target.querySelector("input").getAttribute("id"));
-        console.log(e.target.querySelector("input").value);
+        const time = e.target.querySelector("input").getAttribute("id");
+        const text = e.target.querySelector("input").value;
+
+        window.localStorage.setItem(time, text)
     })
 
 });
